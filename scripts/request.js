43e8b26ef16bf6aef326be181027918e90292886
K Sergey <kurluta.sergey@gmail.com>
2019-03-20 23:13:27 +0300

    Fix some elements location problem,Add JavaScript for element fade

--- scripts/request.js
+++ scripts/request.js
@@ -1,9 +1,13 @@
 $(document).ready(function () {
     $('#request').click(function () {
         $('.form-input').toggleClass('visible');
+        $('.button-wrapper-left').toggleClass('show');
+        $('.button-wrapper-right').toggleClass('show');
     });
 
     $('#close-request').click(function () {
         $('.form-input').removeClass('visible');
+      $('.button-wrapper-left').removeClass('show');
+      $('.button-wrapper-right').removeClass('show');
     });
 });
